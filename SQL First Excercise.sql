1404c5fcaa5a04c12f56abe4377b9f8bc83af9f3
Douglas <douglas-brumley@hotmail.com>
2024-06-27 11:46:07 -0400

    Error fixed

--- SQL First Excercise.sql
+++ SQL First Excercise.sql
@@ -7,7 +7,7 @@ SELECT * FROM bestbuy.products as p WHERE p.price = 11.99 OR p.price = 13.99;
 -- find all products that do NOT cost 11.99 - using NOT
 SELECT * FROM bestbuy.products as p WHERE NOT p.price = 11.99;
 -- find all products and sort them by price from greatest to least
-SELECT * FROM bestbuy. products as p ORDER BY p.price DESC;
+SELECT * FROM bestbuy.products as p ORDER BY p.price DESC;
 -- find all employees who don't have a middle initial
 SELECT * FROM bestbuy.employees WHERE MiddleInitial IS NULL;
 -- find distinct product prices
@@ -19,7 +19,7 @@ SELECT * FROM bestbuy.products WHERE NAME = 'MACBOOK';
 -- find all products that are on sale
 SELECT * FROM bestbuy.products WHERE OnSale = 1;
 -- find the average price of all products
-SELECT AVG(products.price) FROM bestbut.products;
+SELECT AVG(products.price) FROM bestbuy.products;
 -- find all Geek Squad employees who don't have a middle initial
 SELECT * FROM bestbuy.employees AS e WHERE e.MiddleInitial IS NULL AND e.title = 'Geek Squad';
 -- find all products from the products table whose stock level is in the range -- of 500 to 1200. Order by Price from least to greatest
